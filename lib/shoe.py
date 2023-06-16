da8d6eb2a5e4324de4818b4bcf481de702a05519
Macolister <bispo.mac@gmail.com>
2023-06-16 14:47:50 -0400

    write logic for classes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
-class Shoe:
-    pass
+class Shoe():
+
+    
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = " "
+
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+        
